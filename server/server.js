1235b2b014ed050823ed5e055063320348424a43
haifaarouri <haifa.arouri@esprit.tn>
2024-12-06 13:11:36 +0100

    change port to .env + change delete catch error

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // Middleware to parse the request.body : allows us to accept json data in req.body
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server starts at http://localhost:5000");
+  console.log(`Server starts at http://localhost:${PORT}`);
 });
