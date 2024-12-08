ad7ab9b3e8adad18d59ccd07e83b48161e55f6f7
haifaarouri <haifa.arouri@esprit.tn>
2024-12-08 12:21:08 +0100

    add CRUD on frontend

--- server/server.js
+++ server/server.js
@@ -2,11 +2,17 @@ import express from "express";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+const __dirname = path.resolve();
 
 app.use(express.json()); // Middleware to parse the request.body : allows us to accept json data in req.body
 
